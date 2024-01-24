b8e10a1cc25e242911d93ac0752cc2986849a9c7
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2024-01-24 13:00:07 +0200

    Update

--- Python Advanced/(E) Functions Advanced/Lab/05_operate.py
+++ Python Advanced/(E) Functions Advanced/Lab/05_operate.py
@@ -2,7 +2,15 @@ from functools import reduce
 
 
 def operate(operator, *args):
-    return reduce(lambda x, y: eval(f'{x}{operator}{y}'), args)
+
+    if operator == '+':
+        return reduce(lambda x, y: x + y, args)
+    elif operator == '-':
+        return reduce(lambda x, y: x - y, args)
+    elif operator == '*':
+        return reduce(lambda x, y: x * y, args)
+    elif operator == '/':
+        return reduce(lambda x, y: x / y, args)
 
 
 print(operate("+", 1, 2, 3))
