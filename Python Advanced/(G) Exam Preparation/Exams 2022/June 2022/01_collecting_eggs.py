c289729965b5cad5c76433ab727b83216611eb94
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2024-02-01 12:09:48 +0200

    Create solutions for Exam preparation from June 2022

--- Python Advanced/(G) Exam Preparation/Exams 2022/June 2022/01_collecting_eggs.py
+++ Python Advanced/(G) Exam Preparation/Exams 2022/June 2022/01_collecting_eggs.py
@@ -9,21 +9,27 @@ while egg_size and paper_size:
     if egg_size[0] <= 0:
         egg_size.popleft()
         continue
+
     if egg_size[0] == 13:
         egg_size.popleft()
         first = paper_size.popleft()
         paper_size.rotate(1)
         paper_size.append(first)
         continue
+
     result = egg_size.popleft() + paper_size.pop()
+
     if result <= 50:
         filled_boxes += 1
 
 if filled_boxes > 0:
     print(f'Great! You filled {filled_boxes} boxes.')
+
 else:
     print('Sorry! You couldn\'t fill any boxes!')
+
 if egg_size:
     print(f'Eggs left: {", ".join(str(el) for el in egg_size)}')
+
 if paper_size:
     print(f'Pieces of paper left: {", ".join(str(el) for el in paper_size)}')
