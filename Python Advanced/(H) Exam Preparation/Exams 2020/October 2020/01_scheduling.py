662436f28bb1c6416c0ecb527a0e1ba12fe8663f
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2024-02-05 10:41:01 +0200

    update

--- Python Advanced/(H) Exam Preparation/Exams 2020/October 2020/01_scheduling.py
+++ Python Advanced/(H) Exam Preparation/Exams 2020/October 2020/01_scheduling.py
@@ -1,10 +1,9 @@
-
-
 jobs = [int(el) for el in input().split(', ')]
 searched_index = int(input())
 total_clock_cycles = 0
 index_found = False
 jobs_copy = jobs.copy()
+
 while jobs_copy:
     task = min(jobs_copy)
 
@@ -18,13 +17,17 @@ while jobs_copy:
             if job == searched_index:
                 index_found = True
                 break
+
             jobs_copy.remove(task)
 
     else:
         total_clock_cycles += task
+
         if jobs.index(task) == searched_index:
             index_found = True
+
         jobs_copy.remove(task)
+
     if index_found:
         print(total_clock_cycles)
         break
