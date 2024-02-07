d63fc87c235a1cd745b53710556d3d31a7851e67
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2024-02-07 20:19:27 +0200

    Solution for 4.Mathematical Operations

---
+++ 09_modules/lab/mathematical_operations/main.py
@@ -0,0 +1,6 @@
+
+from source import math_operation
+equation = input().split()
+
+
+print(f'{math_operation(equation):.2f}')
---
+++ 09_modules/lab/mathematical_operations/source.py
@@ -0,0 +1,36 @@
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return "Zero can\'t be divider"
+
+
+def power(num1, num2):
+    return pow(num1, num2)
+
+
+operations = {'+': add,
+              '-': subtract,
+              '*': multiply,
+              '/': divide,
+              '^': power}
+
+
+def math_operation(equation):
+    operator = equation[1]
+    first_num = float(equation[0])
+    second_num = int(equation[2])
+
+    return operations[operator](first_num, second_num)
