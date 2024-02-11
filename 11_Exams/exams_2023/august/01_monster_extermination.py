8ab174f5b49f162c9b45a6b8da3655ae9a84b9a3
elizazavlieva <145527176+elizazavlieva@users.noreply.github.com>
2024-02-11 18:41:07 +0200

    Update

--- 11_Exams/exams_2023/august/01_monster_extermination.py
+++ 11_Exams/exams_2023/august/01_monster_extermination.py
@@ -3,14 +3,17 @@ from collections import deque
 monster = deque(int(el) for el in input().split(','))
 striking_impact = [int(el) for el in input().split(',')]
 killed_monsters = 0
+
 while monster and striking_impact:
     if striking_impact[-1] >= monster[0]:
         remaining_impact = striking_impact.pop() - monster.popleft()
         killed_monsters += 1
+
         if len(striking_impact) >= 1 and remaining_impact > 0:
             striking_impact[-1] += remaining_impact
         elif not striking_impact and remaining_impact > 0:
             striking_impact.append(remaining_impact)
+
     else:
         monster[0] -= striking_impact.pop()
         monster.rotate(-1)
--- 11_Exams/exams_2023/august/02_delivery_boy.py
+++ 11_Exams/exams_2023/august/02_delivery_boy.py
@@ -1,47 +1,59 @@
-rows, cols = [int(el) for el in input().split(' ')]
+ROWS, COLS = [int(el) for el in input().split()]
+
 matrix = []
-start_position = tuple()
-
-for row in range(rows):
+start_position = []
+for row in range(ROWS):
     matrix.append(list(input()))
-    for col in range(cols):
+    for col in range(COLS):
         if matrix[row][col] == 'B':
-            start_position = (row, col)
+            start_position = [row, col]
 
-
+current_pos = start_position
 directions = {'up': (-1, 0), 'down': (1, 0),
               'left': (0, -1), 'right': (0, 1)}
 
-delivery_pos = [start_position[0], start_position[1]]
+
+def checking_ranges(move):
+    if move[0] in range(ROWS) and move[1] in range(COLS):
+        return True
+    return False
 
 
+def movement(move, matrix, current_pos, delivered):
+    if matrix[move[0]][move[1]] == 'P':
+        matrix[move[0]][move[1]] = 'R'
+        print('Pizza is collected. 10 minutes for delivery.')
+
+    elif matrix[move[0]][move[1]] == 'A':
+        matrix[move[0]][move[1]] = 'P'
+        delivered = True
+        print('Pizza is delivered on time! Next order...')
+
+    elif matrix[move[0]][move[1]] == '-':
+        matrix[move[0]][move[1]] = '.'
+
+    if matrix[move[0]][move[1]] != '*':
+        current_pos = move
+
+    return matrix, current_pos, delivered
+
+
+delivered = False
+in_range = True
+command = input()
 while True:
-    command = input()
-    if not  command:
-        break
-    move = [delivery_pos[0] + directions[command][0],
-            delivery_pos[1] + directions[command][1]]
-    if move[0] in range(rows) and move[1] in range(cols):
-        if matrix[move[0]][move[1]] == '*':
-            continue
-        if matrix[move[0]][move[1]] == 'P':
-            delivery_pos = move
-            matrix[move[0]][move[1]] = 'R'
-            print('Pizza is collected. 10 minutes for delivery.')
+    move = [current_pos[0] + directions[command][0],
+            current_pos[1] + directions[command][1]]
 
-        if matrix[move[0]][move[1]] == 'A':
-            delivery_pos = move
-            matrix[move[0]][move[1]] = 'P'
-            print('Pizza is delivered on time! Next order...')
+    if checking_ranges(move):
+        matrix, current_pos, delivered = movement(move, matrix, current_pos, delivered)
+        if delivered:
             break
-        if matrix[move[0]][move[1]] == '-':
-            delivery_pos = move
-            matrix[delivery_pos[0]][delivery_pos[1]] = '.'
-
     else:
         print('The delivery is late. Order is canceled.')
-        matrix[start_position[0]][start_position[1]] = ' '
+        matrix[start_position[0]][start_position[1]] = " "
         break
 
+    command = input()
 
-[print(*element, sep='') for element in matrix]
+[print(*elements, sep="") for elements in matrix]
